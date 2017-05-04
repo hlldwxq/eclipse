c2900f619514bbb582e3db02038fa17ce7769caa
wq <826253927@qq.com>
2017-05-04 20:03:13 +0800

    wq eclipse git test

---
+++ src/MyHashMap.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+public class MyHashMap extends AbstractMap<K,V> implements Map<K,V> {
+	
+}
